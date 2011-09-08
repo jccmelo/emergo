9b82be3f8bc570cc8c1391b98ebb79f05f3a59f3
tarsiswt <tarsiswt@fea7a8a5-5848-46ff-b795-545ed800f380>
2011-09-08 20:20:41 +0000

    Overwriting existing Handler.
    git-svn-id: https://subversion.assembla.com/svn/emergo@13 fea7a8a5-5848-46ff-b795-545ed800f380

--- trunk/Emergo - EI/src/br/ufpe/cin/emergo/handlers/EmergoHandler.java
+++ trunk/Emergo - EI/src/br/ufpe/cin/emergo/handlers/EmergoHandler.java
@@ -1,23 +1,40 @@
 package br.ufpe.cin.emergo.handlers;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Set;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.jdt.core.IClasspathEntry;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.Signature;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jface.dialogs.ErrorDialog;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.handlers.HandlerUtil;
-
-import br.ufpe.cin.emergo.markers.EmergoMarker;
-import br.ufpe.cin.emergo.markers.FeatureDependency;
+import org.eclipse.ui.texteditor.IDocumentProvider;
+import org.eclipse.ui.texteditor.ITextEditor;
 
 /**
  * Handler for the br.ufal.cideei.commands.DoCompute extension command.
@@ -27,51 +44,125 @@ import br.ufpe.cin.emergo.markers.FeatureDependency;
  */
 public class EmergoHandler extends AbstractHandler {
 
+	private ICompilationUnit compilationUnit;
+	private CompilationUnit jdtCompilationUnit;
+
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		Map<Object, Object> options = new HashMap<Object, Object>();
+		// XXX Try block for debugging only.
+		try {
+			/*
+			 * Mechanism for passing through information that could make the dependency finder easier/faster to
+			 * implement.
+			 */
+			final Map<Object, Object> options = new HashMap<Object, Object>();
 
-		ISelection selection = HandlerUtil.getCurrentSelectionChecked(event);
-		Shell shell = HandlerUtil.getActiveShellChecked(event);
+			ISelection selection = HandlerUtil.getCurrentSelectionChecked(event);
+			Shell shell = HandlerUtil.getActiveShellChecked(event);
 
-		if (!(selection instanceof ITextSelection))
-			throw new ExecutionException("Not a text selection");
+			if (!(selection instanceof ITextSelection))
+				throw new ExecutionException("Not a text selection");
 
-		/*
-		 * used to find out the project name and later to create a compilation unit from it
-		 */
-		IFile textSelectionFile = (IFile) HandlerUtil.getActiveEditorChecked(event).getEditorInput().getAdapter(IFile.class);
+			IFile textSelectionFile = (IFile) HandlerUtil.getActiveEditorChecked(event).getEditorInput().getAdapter(IFile.class);
+			// ITextSelection textSelection = (ITextSelection) selection;
 
-		/*
-		 * this visitor will compute the ASTNodes that were selected by the user
-		 */
-		ITextSelection textSelection = (ITextSelection) selection;
-		
-		textSelection.getStartLine();
-		textSelection.getOffset();
+			ITextEditor editor = (ITextEditor) HandlerUtil.getActiveEditorChecked(event);
+			IDocumentProvider provider = editor.getDocumentProvider();
+			IDocument document = provider.getDocument(editor.getEditorInput());
+			ITextSelection textSelection = (ITextSelection) editor.getSite().getSelectionProvider().getSelection();
 
-		//TODO: pass the file, start line and offset position to the underlying dependency finder.
-		Logger.getAnonymousLogger().log(Level.INFO, "Generating emergent interface...");
-		testingEmergoResultsView(textSelectionFile);
+			// The project from which the file belongs.
+			IProject project = textSelectionFile.getProject();
 
+			CompilationUnit compilationUnit = getCompilationUnit(textSelectionFile);
+			SelectionNodesVisitor selectionVisitor = new SelectionNodesVisitor(textSelection);
+			compilationUnit.accept(selectionVisitor);
+			Set<ASTNode> nodesInSelection = selectionVisitor.getNodes();
+			MethodDeclaration parentMethod = getParentMethod(nodesInSelection);
+			IMethod method = (IMethod) this.compilationUnit.getElementAt(parentMethod.getStartPosition());
+
+			options.put("type", method.getDeclaringType().getFullyQualifiedName());
+			options.put("methodDescriptor", getMethodDescriptor(method));
+			options.put("method", method.getElementName());
+			
+			/*
+			 * Finds out the (partial) project's classpath as a list of Files. Each File either points to a source
+			 * folder, or an archive like a jar.
+			 */
+			IJavaProject javaProject = JavaCore.create(project);
+			IClasspathEntry[] resolvedClasspath = javaProject.getResolvedClasspath(true);
+			List<File> classpath = new ArrayList<File>();
+			for (IClasspathEntry cpEntry : resolvedClasspath) {
+				switch (cpEntry.getEntryKind()) {
+				case IClasspathEntry.CPE_CONTAINER:
+					classpath.add(cpEntry.getPath().makeAbsolute().toFile());
+					break;
+				case IClasspathEntry.CPE_SOURCE:
+					classpath.add(ResourcesPlugin.getWorkspace().getRoot().getFolder(cpEntry.getPath()).getLocation().toFile());
+				}
+			}
+
+			options.put("classpath", classpath);
+
+			/*
+			 * Holds the textual selection information that needs to passed along to the underlying compiler
+			 * infrastructure
+			 */
+			final SelectionPosition selectionPosition = SelectionPosition.builder().length(textSelection.getLength()).offSet(textSelection.getOffset()).startLine(textSelection.getStartLine()).startColumn(calculateColumnFromOffset(document, textSelection.getOffset())).endLine(textSelection.getEndLine()).endColumn(calculateColumnFromOffset(document, textSelection.getOffset() + textSelection.getLength())).filePath(textSelectionFile.getLocation().toOSString()).build();
+
+			try {
+				DependencyFinder.findFromSelection(DependencyFinderID.JWCOMPILER, selectionPosition, options);
+			} catch (FileNotFoundException e) {
+				ErrorDialog.openError(shell, "Error", "An error has occured", null);
+				e.printStackTrace();
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
-	//TODO: temporary method. Just to test the populate the view to test Emergo GUI functionalities. However, we can reuse part of this method!!!
-	private void testingEmergoResultsView(IFile fileSelected) {
-		
-		try {
-			fileSelected.deleteMarkers(EmergoMarker.EMERGO_MARKER_ID, true, IResource.DEPTH_INFINITE);
-		} catch (CoreException e) {
-			e.printStackTrace();
+	private MethodDeclaration getParentMethod(Set<ASTNode> nodesInSelection) {
+		ASTNode parent;
+		for (ASTNode node : nodesInSelection) {
+			parent = node;
+			do {
+				if (parent.getNodeType() == ASTNode.METHOD_DECLARATION) {
+					return (MethodDeclaration) parent;
+				}
+			} while ((parent = parent.getParent()) != null);
 		}
-		
-		FeatureDependency fd = new FeatureDependency();
-		fd.setLineNumber(32);
-		fd.setFile(fileSelected);
-		fd.setConfiguration("A");
-		fd.setFeature("A");
-
-		EmergoMarker.createMarker("Provides x to " + "metodo(napo)", fd);
+		return null;
 	}
 
+	private CompilationUnit getCompilationUnit(IFile textSelectionFile) {
+		this.compilationUnit = JavaCore.createCompilationUnitFrom(textSelectionFile);
+		ASTParser parser = ASTParser.newParser(AST.JLS3);
+		parser.setSource(compilationUnit);
+		parser.setKind(ASTParser.K_COMPILATION_UNIT);
+		parser.setResolveBindings(true);
+		this.jdtCompilationUnit = (CompilationUnit) parser.createAST(null);
+		return jdtCompilationUnit;
+	}
+
+	private String getMethodDescriptor(IMethod method) throws JavaModelException {
+		return Signature.createMethodSignature(method.getParameterTypes(), method.getReturnType().toString());
+	}
+
+	public int calculateColumnFromOffset(IDocument doc, int offset) {
+		int sumpos = 0;
+		int i = 0;
+
+		try {
+			while (sumpos + doc.getLineLength(i) - 1 < offset) {
+				sumpos += doc.getLineLength(i);
+				++i;
+			}
+		} catch (BadLocationException e) {
+			// XXX ???
+			e.printStackTrace();
+		}
+
+		return offset - sumpos + 1;
+	}
 }
